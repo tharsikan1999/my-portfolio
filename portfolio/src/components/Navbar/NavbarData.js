1af982f2d6a493a68b88089f2de1d166fe6aa704
Sathasivam Tharsikan <110738940+tharsikan1999@users.noreply.github.com>
2024-03-12 12:36:59 +0530

    Delete portfolio directory

--- portfolio/src/components/Navbar/NavbarData.js
+++
@@ -1,9 +0,0 @@
-//---- Start Nvabar Data -----
-export const NavbarData = [
-    { name: "Home", link: "home" },
-    { name: "Services", link: "services" },
-    { name: "Skills", link: "Skills" },
-    { name: "PROJECTS", link: "Portfolio" },
-    { name: "Contact", link: "Contact" },
-]
-//----- End Navbar Data -----
